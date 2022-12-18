9dd2642c734de2fdbc395f4315b51d6e20d1f335
Frankly <frankly.arruda@gmail.com>
2022-12-18 14:34:21 -0300

    loja de jogos projeto

--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -1,91 +1,81 @@
-
+// request via JavaScript ajax 4 passos
+// 01 criar a váriavel
 xhttp = new XMLHttpRequest();
-
-var listaProduto;
+var lista;
 var api = "http://localhost:8080/api/produto/";
 
-function listarProdutos(){
-
+function listar() {
+    // 02 definição do nosso request (forma e endereço)
     xhttp.open("GET", api);
+    // 03 manda de fato a request
     xhttp.send();
-
-    xhttp.onload = function(){
-        listaProduto = this.responseText;
-      //  console.log(listaProduto);
-        listaProduto = JSON.parse(listaProduto);
-       // console.log(listaProduto);    
-
+    // 04 execução quando tiver a devolutiva do request
+    xhttp.onload = function () {
+        lista = this.responseText;
+        // console.log(lista);
+        lista = JSON.parse(lista);
+        // console.log(lista);
         texto = "";
-        
         i = 0;
-        for (const u of listaProduto){
-            texto += `<tr onclick='editar(${i})'>
-                        <td>${u.nome}</td>
-                        <td>${u.descricao}</td>
-                        <td>${u.valor.toFixed(2)}</td>
-                        </tr>`;
+        for (const u of lista) {
+            texto += `<tr onclick='editar(${i})'><td>${u.nome}</td><td>R$ ${u.valor.toFixed(2)}</td></tr>`;
             i++;
         }
-        document.getElementById('listaproduto').innerHTML = texto;
-
+        document.getElementById('lista').innerHTML = texto;
     }
 }
 
-function editar(i){
-
-    u = listaProduto[i];
-    document.getElementById("id").value = u.id;
+function editar(i) {
+    u = lista[i];
     document.getElementById("nome").value = u.nome;
     document.getElementById("descricao").value = u.descricao;
     document.getElementById("valor").value = u.valor;
-
+    document.getElementById("img").value = u.img;
+    document.getElementById("id").value = u.id;
 }
 
-function gravar(){
-    alert("dentro de gravar");
-    var produtos = {};
-    produtos.id = document.getElementById("id").value;
-    produtos.nome = document.getElementById("nome").value;
-    produtos.descricao = document.getElementById("descricao").value;
-    produtos.valor = document.getElementById("valor").value;
-    
-    
+function gravar() {
+    //alert("Estamos dentro da function incluir");
+    var item = {};
+    item.nome = document.getElementById("nome").value;
+    item.descricao = document.getElementById("descricao").value;
+    item.valor = document.getElementById("valor").value;
+    item.img = document.getElementById("img").value;
+    // console.log(item);
 
-if (produtos.id > 0){
-    acao = "PUT"; //alteração
-} else {
-    acao = "POST"; //incluir
+    item.id = document.getElementById("id").value;
+    if (item.id > 0) {
+        acao = "PUT"; // alteração
+    } else {
+        acao = "POST"; // incluir
+    }
 
-}
-
-xhttp.open(acao, api);
-xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-xhttp.send(JSON.stringify(produtos));
-
-xhttp.onload = function (){
-    //console.log(this.responseText);
-    listarProdutos();
-    limpar();
-}
-}
-
-function limpar(){
-    document.getElementById("nome").value = "";
-    document.getElementById("descricao").value = "";
-    document.getElementById("valor").value = "";
-    document.getElementById("id").value = "";
-}
-
-function apagar(){
-    id = document.getElementById("id").value;
-    xhttp.open("DELETE", api + id);
-    xhttp.send();
-    xhttp.onload = function(){
-
-        alert(this.responseText);
-        listarProdutos();
+    xhttp.open(acao, api);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send(JSON.stringify(item));
+    xhttp.onload = function () {
+        // console.log(this.responseText);
+        listar();
         limpar();
     }
 }
-listarProdutos();
 
+function limpar() {
+    document.getElementById("nome").value = "";
+    document.getElementById("descricao").value = "";
+    document.getElementById("valor").value = "";
+    document.getElementById("img").value = "";
+    document.getElementById("id").value = "";
+}
+
+function apagar() {
+    id = document.getElementById("id").value;
+    xhttp.open("DELETE", api + id);
+    xhttp.send();
+    xhttp.onload = function () {
+        alert(this.responseText);
+        listar();
+        limpar();
+    }
+}
+listar();
